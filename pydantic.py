41ec80f0a17139e4417d3c76a2961ae4dfa63aa2
BiploveGC <98416508+BiploveGC@users.noreply.github.com>
2024-12-15 10:06:10 -0800

    Create pydantic.py

---
+++ pydantic.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from datetime import date
+
+class BookBase(BaseModel):
+    title: str
+    author: str
+    published_date: date
+    isbn: str
+    pages: int
+    language: str
+
+class BookCreate(BookBase):
+    pass
+
+class Book(BookBase):
+    id: int
+
+    class Config:
+        orm_mode = True
